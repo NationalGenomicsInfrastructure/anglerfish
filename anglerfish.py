a80c9c0389f3cddefca56b75488b9cc6388fdf0d
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2020-01-29 10:55:11 +0100

    Add more stats and report samples missing BCs

--- anglerfish.py
+++ anglerfish.py
@@ -5,11 +5,12 @@ import argparse
 import logging
 import sys
 import os
+import pkg_resources
 from datetime import datetime as dt
 from itertools import groupby
 from demux.demux import run_minimap2, parse_paf_lines, layout_matches, cluster_matches, write_demuxedfastq, run_fastqc
 from demux.samplesheet import SampleSheet
-from Bio.SeqIO.QualityIO import FastqGeneralIterator
+import gzip
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('anglerfish')
 
@@ -20,6 +21,8 @@ def run_demux(args):
     aln_path = os.path.join(args.out_fastq,"out.paf")
     os.mkdir(args.out_fastq)
     ss = SampleSheet(args.samplesheet)
+    version = pkg_resources.get_distribution("anglerfish").version
+
     bc_dist = ss.minimum_bc_distance()
     if args.max_distance >= bc_dist:
         log.error(" Edit distance of barcodes in samplesheet are less than the minimum specified {}>={}".format(args.max_distance, bc_dist))
@@ -31,6 +34,13 @@ def run_demux(args):
         f.write(ss.get_fastastring())
     retcode = run_minimap2(args.in_fastq, adaptor_path, aln_path, args.threads)
 
+    # Quick line count in input fastqfile
+    fq_entries = 0
+    with gzip.open(args.in_fastq, 'rb') as f:
+        for i in f:
+            fq_entries += 1
+    fq_entries = int(fq_entries / 4)
+
     # Sort the adaptors by type and size
     adaptors_t = [adaptor.name for sample, adaptor in ss]
     adaptor_set = set(adaptors_t)
@@ -40,30 +50,37 @@ def run_demux(args):
 
     paf_entries = parse_paf_lines(aln_path)
     out_fastqs = []
-    stats = [args.out_fastq, "=================",""]
+    stats = ["Anglerfish v. "+version, "===================",""]
     for adaptor in adaptor_set:
         fragments, singletons, concats, unknowns = layout_matches(adaptor+"_i5",adaptor+"_i7",paf_entries)
         total = len(fragments)+len(singletons)+len(concats)+len(unknowns)
         stats.append(adaptor+":")
+        stats.append("{}\tinput reads".format(fq_entries))
+        stats.append("{}\treads aligning to adaptor sequences ({:.2f}%)".format(total, (total/float(fq_entries)*100)))
         stats.append("{}\treads matching both I7 and I5 adaptor ({:.2f}%)".format(len(fragments), (len(fragments)/float(total)*100)))
         stats.append("{}\treads matching only I7 or I5 adaptor ({:.2f}%)".format(len(singletons), (len(singletons)/float(total)*100)))
         stats.append("{}\treads matching multiple I7/I5 adaptor pairs ({:.2f}%)".format(len(concats), (len(concats)/float(total)*100)))
-        stats.append("{}\treads with uncategorized matches ({:.2f}%)".format(len(unknowns), (len(unknowns)/float(total)*100)))
+        stats.append("{}\treads with uncategorized alignments ({:.2f}%)".format(len(unknowns), (len(unknowns)/float(total)*100)))
         matches = cluster_matches(adaptors_sorted[adaptor], adaptor, fragments, args.max_distance)
         stats.append("")
         stats.append("sample_name\t#reads")
-        if not args.skip_demux:
-            for k, v in groupby(sorted(matches,key=lambda x: x[3]), key=lambda y: y[3]):
-                fq_name = os.path.join(args.out_fastq, k+".fastq.gz")
-                out_fastqs.append(fq_name)
-                sample_dict = {i[0]: [i] for i in v}
-                stats.append("{}\t{}".format(k, len(sample_dict.keys())))
+        aligned_samples = []
+        for k, v in groupby(sorted(matches,key=lambda x: x[3]), key=lambda y: y[3]):
+            aligned_samples.append(k)
+            fq_name = os.path.join(args.out_fastq, k+".fastq.gz")
+            out_fastqs.append(fq_name)
+            sample_dict = {i[0]: [i] for i in v}
+            stats.append("{}\t{}".format(k, len(sample_dict.keys())))
+            if not args.skip_demux:
                 write_demuxedfastq(sample_dict, args.in_fastq, fq_name)
-
+        # Check if there were samples in the samplesheet without adaptor alignments
+        for ss_sample, ss_adaptor in ss:
+            if ss_adaptor.name == adaptor and ss_sample not in aligned_samples:
+                stats.append("{}\t0".format(ss_sample))
     with open(os.path.join(args.out_fastq,"anglerfish_stats.txt"), "w") as f:
         for i in stats:
             f.write(i+"\n")
-            print(i)
+            log.info(i)
     if not args.skip_fastqc and not args.skip_demux:
         fastqc = run_fastqc(out_fastqs, args.out_fastq, args.threads)
 
@@ -81,11 +98,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
     utcnow = dt.utcnow()
     runname = utcnow.strftime("anglerfish_%Y_%m_%d_%H%M%S")
+
     assert os.path.exists(args.out_fastq)
     assert os.path.exists(args.in_fastq)
     assert os.path.exists(args.samplesheet)
     args.out_fastq = os.path.join(os.path.abspath(args.out_fastq),runname)
     args.in_fastq = os.path.abspath(args.in_fastq)
     args.samplesheet = os.path.abspath(args.samplesheet)
-    # TODO: input validation
     run_demux(args)
