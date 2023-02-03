8e5591e82b63701ca9354e123fa7863a8bf7a224
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2023-02-03 16:07:13 +0100

    Add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
+"""
+Anglerfish is a tool designed to demultiplex Illumina libraries sequenced on Oxford Nanopore flowcells. The primary purpose for this would be to do QC, i.e. to check pool balancing, assess contamination, library insert sizes and so on.
+
+Install with pip:
+
+    pip install bio-anglerfish
+
+Or from bioconda:
+
+    conda install -c bioconda anglerfish
+"""
 from setuptools import setup, find_packages
 import sys, os
 
+version='0.4.2'
+
 setup(
     name='bio-anglerfish',
-    version='0.4.2',
+    version=version,
     description='Anglerfish, a tool to demultiplex Illumina libraries from ONT data',
+    long_description=__doc__,
     author='Remi-Andre Olsen',
     author_email='remi-andre.olsen@scilifelab.se',
     url='https://github.com/remiolsen/anglerfish',
