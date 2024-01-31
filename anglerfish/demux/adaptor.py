92483fad3951e05ab2a6f459eaa58596dacfe833
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2024-01-31 17:24:46 +0100

    Slowly unbreaking merge

--- anglerfish/demux/adaptor.py
+++ anglerfish/demux/adaptor.py
@@ -15,10 +15,10 @@ class Adaptor:
         self.i7 = AdaptorPart(adaptors[adaptor]["i5"], adaptor, delim, i5_index)
         self.i5_index = i5_index
         self.i7_index = i7_index
-        self.i5_umi = re.findall(udelim, self.i5)
+        self.i5_umi = re.findall(udelim, self.i5.sequence)
         self.i5_umi_before = 0
         self.i5_umi_after = 0
-        self.i7_umi = re.findall(udelim, self.i7)
+        self.i7_umi = re.findall(udelim, self.i7.sequence)
         self.i7_umi_before = 0
         self.i7_umi_after = 0
         self.name = f"{adaptor}"
@@ -29,31 +29,31 @@ class Adaptor:
                 f"Adaptor {adaptor} has more than one UMI in either i5 or i7. This is not supported."
             )
         # Check if UMI is before or after i5 index
-        if len(self.i5_umi) > 0 and ">" + self.i5_umi[0] in self.i5:
+        if len(self.i5_umi) > 0 and ">" + self.i5_umi[0] in self.i5.sequence:
             self.i5_umi_after = int(re.search(ulen, self.i5_umi[0]).group(1))
-        elif len(self.i5_umi) > 0 and self.i5_umi[0] + "<" in self.i5:
+        elif len(self.i5_umi) > 0 and self.i5_umi[0] + "<" in self.i5.sequence:
             self.i5_umi_before = int(re.search(ulen, self.i5_umi[0]).group(1))
         elif len(self.i5_umi) > 0:
             raise UserWarning(
                 f"Adaptor {adaptor} has UMI but it does not flank an index. This is not supported."
             )
         # Check if UMI is before or after i7 index
-        if len(self.i7_umi) > 0 and ">" + self.i7_umi[0] in self.i7:
+        if len(self.i7_umi) > 0 and ">" + self.i7_umi[0] in self.i7.sequence:
             self.i7_umi_after = int(re.search(ulen, self.i7_umi[0]).group(1))
-        elif len(self.i7_umi) > 0 and self.i7_umi[0] + "<" in self.i7:
+        elif len(self.i7_umi) > 0 and self.i7_umi[0] + "<" in self.i7.sequence:
             self.i7_umi_before = int(re.search(ulen, self.i7_umi[0]).group(1))
         elif len(self.i7_umi) > 0:
             raise UserWarning(
                 f"Adaptor {adaptor} has UMI but it does not flank an index. This is not supported."
             )
-        if re.search(idelim, self.i5) is not None and i5_index is None:
+        if re.search(idelim, self.i5.sequence) is not None and i5_index is None:
             raise UserWarning("Adaptor has i5 but no sequence was specified")
-        if re.search(idelim, self.i7) is not None and i7_index is None:
+        if re.search(idelim, self.i7.sequence) is not None and i7_index is None:
             raise UserWarning("Adaptor has i7 but no sequence was specified")
 
     def get_i5_mask(self):
         if self.i5_index is not None:
-            new_i5 = re.sub(idelim, "N" * len(self.i5_index), self.i5)
+            new_i5 = re.sub(idelim, "N" * len(self.i5_index), self.i5.sequence)
             new_i5 = re.sub(
                 udelim, "N" * max(self.i5_umi_after, self.i5_umi_before), new_i5
             )
@@ -63,7 +63,7 @@ class Adaptor:
 
     def get_i7_mask(self):
         if self.i7_index is not None:
-            new_i7 = re.sub(idelim, "N" * len(self.i7_index), self.i7)
+            new_i7 = re.sub(idelim, "N" * len(self.i7_index), self.i7.sequence)
             new_i7 = re.sub(
                 udelim, "N" * max(self.i7_umi_after, self.i7_umi_before), new_i7
             )
@@ -111,8 +111,9 @@ def load_adaptors(raw=False):
         return adaptors_raw
     adaptors = []
     for adaptor in adaptors_raw:
+        # This is now broken, I think
         adaptors.append(
-            Adaptor(adaptors_raw, "-NNN-", adaptor, i7_index=None, i5_index=None)
+            Adaptor(adaptors_raw, "<N>", adaptor, i7_index=None, i5_index=None)
         )
 
     return adaptors
