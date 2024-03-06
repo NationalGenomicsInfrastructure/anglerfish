e119808fbce679f16381e1e77dc60f8d8230098f
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2024-03-06 11:02:51 +0100

    Bump to dev version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-version = "0.6.1"
+version = "0.7.0dev0"
 
 setup(
     name="bio-anglerfish",
