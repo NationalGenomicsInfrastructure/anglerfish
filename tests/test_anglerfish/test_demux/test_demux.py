a8c335cb867b8f180eb5c41cccd0ccbe81b9c6fd
kedhammar <alfred.kedhammar@scilifelab.se>
2024-06-04 09:15:54 +0000

    improve docs

--- tests/test_anglerfish/test_demux/test_demux.py
+++ tests/test_anglerfish/test_demux/test_demux.py
@@ -9,11 +9,20 @@ from anglerfish.demux import demux as to_test
 
 @pytest.fixture(scope="module")
 def fixture():
-    """Create a temporary directory shared between all tests in this module."""
+    f"""Fixture for all tests within {__file__}.
+    
+    Creats a temporary directory with...
+    - testdata.fastq.gz: A single read subset from the testdata with a perfect index match to the truseq i7 adaptor.
+    - truseq.fasta: A fasta file with the truseq i7 and i5 adaptors.
+
+
+    Expected files to be created...
+    - test.paf: The output of minimap2 run on the testdata.fastq.gz and truseq.fasta.
+    """
     tmp = tempfile.TemporaryDirectory()
     tmp_path = Path(tmp.name)
 
-    # Create subset testdata, a single read subset from the testdata
+    # Create test reads
     with gzip.open(tmp_path / "testdata.fastq.gz", "wb") as f:
         f.write(b"""
 @0ad8bdb6-e009-43c5-95b1-d381e699f983 runid=7d98ab9ee7f513f92fd5e382dc3dd563b956e4fa sampleid=Pools read=7283 ch=21 start_time=2020-01-17T15:19:22Z
@@ -23,7 +32,7 @@ CGTTGTAGTTCACCAAACCCAACAACCTAGATAGGCCGCACCTAAAATGATACGGCGACCGCGAGATCCCGATTCTGACA
 
 """)
 
-    # Create appropriate index file
+    # Create adaptor fasta file
     with open(tmp_path / "truseq.fasta", "w") as f:
         f.write(""">truseq_i7
 GATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNNNNNATCTCGTATGCCGTCTTCTGCTTG
@@ -56,7 +65,6 @@ def test_run_minimap2(fixture):
 
     expected = "0ad8bdb6-e009-43c5-95b1-d381e699f983\t418\t302\t374\t+\ttruseq_i7\t67\t0\t67\t51\t64\t25\tNM:i:23\tms:i:275\tAS:i:266\tnn:i:10\ttp:A:P\tcm:i:5\ts1:i:29\ts2:i:0\tde:f:0.2388\trl:i:0\tcg:Z:11M2D33M7I21M\tcs:Z::11-ca:6*tg:13*nt*na*na*nc*nt*nt*ng*ng*nt*nc:1*ta:1+ctagaaa:2*gt*tg:17\n0ad8bdb6-e009-43c5-95b1-d381e699f983\t418\t45\t110\t+\ttruseq_i5\t58\t0\t58\t56\t66\t38\tNM:i:10\tms:i:313\tAS:i:305\tnn:i:0\ttp:A:P\tcm:i:10\ts1:i:37\ts2:i:0\tde:f:0.0667\trl:i:0\tcg:Z:15M1D6M7I3M1I33M\tcs:Z::15-a*cg:5+tcccgat:3+g:33\n"
     received = open(fixture["paf"]).read()
-
     assert expected == received
 
 
